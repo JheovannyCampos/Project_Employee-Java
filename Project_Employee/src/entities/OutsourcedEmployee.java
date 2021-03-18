ab22ef329cf3b40c3b0d434883b2be208be75de7
JheovannyCampos <jheovanny06@hotmail.com>
2021-03-18 16:31:40 -0300

    Add files via upload

---
+++ Project_Employee/src/entities/OutsourcedEmployee.java
@@ -0,0 +1,28 @@
+package entities;
+
+public class OutsourcedEmployee extends Employee{
+	
+	private double additionalCharge;
+	
+	public OutsourcedEmployee() {
+		super();
+	}
+
+	public OutsourcedEmployee(String name, Integer hours, double valuePerHour, double additionalCharge) {
+		super(name, hours, valuePerHour);
+		this.additionalCharge = additionalCharge;
+	}
+
+	public double getAdditionalCharge() {
+		return additionalCharge;
+	}
+
+	public void setAdditionalCharge(double additionalCharge) {
+		this.additionalCharge = additionalCharge;
+	}
+	
+	@Override
+	public double payment() {
+		return super.payment() + additionalCharge * 1.1;
+	}
+}
